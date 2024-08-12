5a2f0125e19494d457eb8a7e3a5cf9a8fd527269
pratikshya swain <pratikshyaswain224@gmail.com>
2024-08-12 19:01:12 +0530

    thirdCommit

--- src/components/WordPart.jsx
+++ src/components/WordPart.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Header from "./Header";
 import SearchForm from "./SearchForm";
-import Footer from "./Footer";
 
 const WordPart = () => {
   return (
     <div className="HomeBody">
       <Header></Header>
       <SearchForm></SearchForm>
-      {/* <Footer></Footer> */}
     </div>
   );
 };
